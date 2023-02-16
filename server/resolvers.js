4d6ae60b7dc95621a99c75c70f963fb6ac2af7be
Shawn Johnson <johnson.sg@gmail.com>
2023-02-15 23:47:11 -0500

    Job-Company Association

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,13 @@
-import { Job } from './db.js'
+import { Company, Job } from './db.js'
 
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll()
     // Don't need async because Job.findAll() already returns a Promise.
+  },
+
+  Job: {
+    // console.log('resolving company for jobs:', job)
+    company: job => Company.findById(job.companyId)
   }
 }
