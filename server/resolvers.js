180f685a6627ceca43d68e2d3627f8381adad4ef
Shawn Johnson <johnson.sg@gmail.com>
2023-02-15 22:50:50 -0500

    Database Resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,8 @@
+import { Job } from './db.js'
+
 export const resolvers = {
   Query: {
-    jobs: () => []
+    jobs: () => Job.findAll()
+    // Don't need async because Job.findAll() already returns a Promise.
   }
 }
